e0b22b37c14c16a51c9867bfc85f7b5f6bd86ca0
Linx07 <1778913273@qq.com>
2026-09-16 21:36:29 +0800

    feat: 练习CSS与JS三种引入方式

---
+++ j1.js
@@ -0,0 +1,7 @@
+// ============ 外链式引入 JavaScript ============
+// 这个文件被 index.html 里的 <script src="./j1.js"> 接入
+
+console.log("外链式引入 javascript");
+
+// 小演示：加载后把浏览器标签页标题改一下，证明 JS 生效了
+document.title = "林鑫的个人简历 ✔";
